37cd4c00d50016da17786a4125037441a346a52c
Shivam Kumar <144941105+Mishrashivam66@users.noreply.github.com>
2026-06-04 23:14:45 +0530

    Remove server port configuration and listener

--- server.js
+++ server.js
@@ -135,8 +135,4 @@ setInterval(
 // SERVER
 // =====================================
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
