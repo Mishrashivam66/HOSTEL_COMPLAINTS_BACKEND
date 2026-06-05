417f1ac089e45c50ef2de329abebd67fb90e75c8
Shivam Kumar <144941105+Mishrashivam66@users.noreply.github.com>
2026-06-05 07:10:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-
+const express = require("express");
+const cors = require("cors");
 
 const dotenv = require("dotenv");
 
@@ -39,9 +40,6 @@ connectDB();
 // =====================================
 // CORS
 // =====================================
-
-const express = require("express");
-const cors = require("cors");
 
 const app = express();
 
